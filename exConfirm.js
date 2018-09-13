1c1b80cf048d414b163c99f46a220bbc365a593d
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2018-09-13 11:29:01 +0600

    Update exConfirm.js

--- exConfirm.js
+++ exConfirm.js
@@ -6,20 +6,25 @@
 let exConfirmPromiseVal = null;
 let exConfirmPromiseInterval = null;
 let exConfirmPromise = {
-    "make": function (options) {
+    "make": function (optionsParam) {
         //setting default value
-        options = Object.assign({
-            bodyBackground: "rgba(255,255,255,1)",
-            titleBackground: "rgba(66, 139, 202, 1)",
-            textColor: "#000000",
-            titleColor: "#ffffff",
-            btnPosition: "right",
-            top: "25%",
-            btnClassSuccess: "btn btn-success btn-sm",
-            btnClassFail: "btn btn-danger btn-sm",
-            title: "Confirmation",
-            message: "Confirmation Required!"
-        }, options);
+        options = {
+            overlayBackground : "rgba(255,255,255,0)", //String [HEX, RGB, RGBA]
+            bodyBackground: "rgba(255,255,255,1)", //String [HEX, RGB, RGBA]
+            titleBackground: "rgba(66, 139, 202, 1)", //String [HEX, RGB, RGBA]
+            textColor: "#000000", //String [HEX, RGB, RGBA]
+            titleColor: "#ffffff", //String [HEX, RGB, RGBA]
+            btnPosition: "right", //String [left, center, right]
+            top: "25%", //String [px, %]
+            right: "38%", //String [px, %]
+            btnClassSuccess: "btn btn-success btn-sm", //String
+            btnClassFail: "btn btn-danger btn-sm", //String
+            title: "Confirmation", //String
+            message: "Confirmation Required!" //String
+        };
+        optionsParam = typeof optionsParam == "undefined" ? {} : optionsParam;
+        
+        options = Object.assign(options, optionsParam);
         exConfirmPromise.doReset();
 
         //prepaer inner html
@@ -27,7 +32,7 @@ let exConfirmPromise = {
                                                 'background:'+options.bodyBackground+';'+
                                                 'position: fixed;'+
                                                 'top: ' + options.top + ';' +
-                                                'right: 35%;' +
+                                                'right: ' + options.right + ';' +
                                                 'width:300px;'+
                                                 'cursor: pointer;'+
                                                 'overflow: hidden;'+
@@ -42,17 +47,38 @@ let exConfirmPromise = {
                                                 'z-index: 99999;">'+
                        '<div id="exConfirmPromise_title" style="height: 26px;padding: 4px 0px 0px 10px;background:' + options.titleBackground + ';color:' + options.titleColor + ';">' + options.title + '</div>' +
                        '<p style="text-align: left;padding: 10px;width: 100%;margin: 0;color:' + options.textColor + '">' + options.message + '</p>' +
-                       '<div style="padding: 10px;text-align: ' + options.btnPosition + ';">' +
-                        '<button onclick="exConfirmPromise.resolve();" style=" width: 62px;" class="' + options.btnClassSuccess + '" autofocus> Yes</button> ' +
-                        '<button onclick="exConfirmPromise.reject();" style=" width: 62px;" class="' + options.btnClassFail + '" > No</button> ' +
+                       '<div id="exConfirmPromiseBtnWrap" style="padding: 10px;text-align: ' + options.btnPosition + ';">' +                        
                        '</div>' +
                    '</div>';
+        // button click function
+        let exResolveBtn = document.createElement("button");
+        exResolveBtn.setAttribute("style", "width: 62px;");
+        exResolveBtn.setAttribute("class", options.btnClassSuccess);
+        exResolveBtn.setAttribute("autofocus","");
+        exResolveBtn.innerHTML="Yes";
+        exResolveBtn.addEventListener("click",(event) =>{
+            exConfirmPromise.resolve();
+        });
+        // space between bbtton
+        let exBtnDivSpace = document.createTextNode(" ");
+
+        let exRejectBtn = document.createElement("button");
+        exRejectBtn.setAttribute("style", "width: 62px;");
+        exRejectBtn.setAttribute("class", options.btnClassFail);
+        exRejectBtn.innerHTML="No";
+        exRejectBtn.addEventListener("click",(event) =>{
+            exConfirmPromise.reject();
+        });
         //append inner html
-        exMainDiv = document.createElement("div");
+        let exMainDiv = document.createElement("div");
         exMainDiv.setAttribute("id", "exConfirmPromiseOverLay");
-        exMainDiv.setAttribute("style", "position:fixed;top:0;left:0;width:100%;height:100%;z-index:99998;");
+        exMainDiv.setAttribute("style", "position:fixed;top:0;left:0;width:100%;height:100%;z-index:99998;background:"+options.overlayBackground+";");
         exMainDiv.innerHTML = htmlDiv
-        document.querySelector("body").appendChild(exMainDiv);
+        document.querySelector("body").appendChild(exMainDiv);        
+        //append btn
+        document.querySelector("#exConfirmPromiseBtnWrap").appendChild(exResolveBtn);
+        document.querySelector("#exConfirmPromiseBtnWrap").appendChild(exBtnDivSpace);
+        document.querySelector("#exConfirmPromiseBtnWrap").appendChild(exRejectBtn);
 
         //return promise
         return new Promise((resolve, reject) => {
@@ -89,4 +115,3 @@ document.addEventListener('keydown', (event) => {
         exConfirmPromise.doReset();
     }
 });
-
